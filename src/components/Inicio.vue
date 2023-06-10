94e6ea06b5dbeff6548a2e9a03f5741e8fdeb1d0
Raul2028 <raulrangel942@gmail.com>
2023-06-09 22:05:05 -0500

    Agregando carpeta al repositorio

---
+++ src/components/Inicio.vue
@@ -0,0 +1,10 @@
+<template> 
+<div> 
+    <h1>Este es el componente {{ name }}</h1> 
+</div> 
+</template> 
+
+<script> 
+export default { name: '{{ MyName }}' }; 
+</script> 
+<style> </style>
---
+++ src/components/Opcion2.vue
@@ -0,0 +1,10 @@
+<template> 
+    <div> 
+        <h1>Este es el componente {{ name }}</h1> 
+    </div> 
+    </template> 
+    
+    <script>
+    export default { name: '{{ MyNname }}' }; 
+    </script> 
+    <style> </style>
